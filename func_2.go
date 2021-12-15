fdb32312f9592c72844da0136ab42b2f43d3aa10
gchen <gchen3544@gmail.com>
2021-12-15 11:04:18 +0800

    新增方法

---
+++ func_2.go
@@ -0,0 +1,19 @@
+package cgghui
+
+import (
+	"crypto/rand"
+	"math"
+	"math/big"
+)
+
+// RangeRand 随机数
+func RangeRand(min, max int64) int64 {
+	if min < 0 {
+		f64Min := math.Abs(float64(min))
+		i64Min := int64(f64Min)
+		result, _ := rand.Int(rand.Reader, big.NewInt(max+1+i64Min))
+		return result.Int64() - i64Min
+	}
+	result, _ := rand.Int(rand.Reader, big.NewInt(max-min+1))
+	return min + result.Int64()
+}
